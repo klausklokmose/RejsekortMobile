a42c438d8415f75b5e7a6a0af61a28f4d0974f64
Cem Turan <cem2ran@gmail.com>
2013-11-23 21:20:39 +0100

    Added RejsekortMobileServer [Python]

---
+++ RejsekortMobileServer/src/RPMServer/journey.py
@@ -0,0 +1,19 @@
+'''
+Created on 23/11/2013
+
+@author: cem2ran
+'''
+from stationstop import StationStop
+
+class Journey(object):
+    '''
+    classdocs
+    '''
+    wayPoints = []
+
+    def __init__(self, StationID):
+        '''
+        Constructor
+        '''
+    def addWaypoint(self, sid):
+        self.wayPoints.append() 
---
+++ RejsekortMobileServer/src/RPMServer/traveler.py
@@ -0,0 +1,38 @@
+'''
+Created on 21/11/2013
+
+@author: cem2ran
+'''
+from journey import Journey
+
+class Traveler(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self,ID=-1, name=None, journey=None):
+        '''
+        Constructor
+        '''
+        self.ID = ID
+        self.name = name
+        self.journey = journey
+        self.journies = []
+        self.checkedIn = False
+        
+    def checkIn(self, stationID):
+        if(self.isCheckedIn()):
+            self.journey.addWaypoint(stationID)
+        else:
+            self.journey = Journey(stationID)
+        self.checkedIn = True
+    def checkOut(self):
+        #db insert 
+        self.journies.append(self.journey);
+        self.journey = None;
+        self.checkedIn = False;
+    def isCheckedIn(self):
+        return True if (self.journey != None and self.checkedIn == True) else False
+
+        
+        
