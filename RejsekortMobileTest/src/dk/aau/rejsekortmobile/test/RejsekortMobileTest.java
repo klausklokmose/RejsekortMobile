2dba466156573366813cc8162e46c45151f44f1c
Klaus Klokmose Nielsen <kkni11@student.aau.dk>
2013-12-09 11:44:09 +0100

    Revert "Revert "test case 1""
    This reverts commit f77111230549c79221e4043d93c01b32ddde4444.

---
+++ RejsekortMobileTest/src/dk/aau/rejsekortmobile/test/RejsekortMobileTest.java
@@ -0,0 +1,40 @@
+package dk.aau.rejsekortmobile.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+import dk.aau.rejsekortmobile.MainActivity_;
+
+public class RejsekortMobileTest extends
+		ActivityInstrumentationTestCase2<MainActivity_> {
+
+	private MainActivity_ mainActiv;
+	private TextView loginText;
+	private String resourceString;
+
+	public RejsekortMobileTest() {
+		super("dk.aau.rejsekortmobile", MainActivity_.class);
+	}
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mainActiv = this.getActivity();
+		loginText = (TextView) mainActiv.findViewById(dk.aau.rejsekortmobile.R.id.loginText);
+		resourceString = "Checked in";
+		try {
+			runTestOnUiThread(new Runnable() {
+			    @Override
+			    public void run() {
+			      loginText.setText(resourceString);
+			    }
+			  });
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+	public void testPreconditions() {
+		assertNotNull(loginText);
+	}
+	public void testText() {
+		assertEquals(resourceString, loginText.getText());
+	}
+}
